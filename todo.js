705afc0923194914113eb72d90715001fdb1f8e5
mubashir <mubashirsono942@gmail.com>
2024-05-17 17:49:18 +0500

    first commit

---
+++ todo.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "what do you want to add in your Todos?"
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Are you sure to want add more?",
+            default: "false"
+        }
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todos);
+}
